7ab522073e65c0860870a9c8e0b68970c949b53e
Aidana <aidanaalhomsi@gmail.com>
2020-10-24 14:59:16 -0400

    JavaCode-Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,12 +1,33 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class Java implements Course {
     @Override
     public void getTeachingHours() {
         System.out.println("Java Weekly teaching hours: 20");
+    }
+
+    @Value("JD1")
+    private String batch;
+
+    @Value("${days}")
+    private String[]days;
+
+    @Value("${instructor")
+    private String instructor;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", days=" + Arrays.toString(days) +
+                ", instructor='" + instructor + '\'' +
+                '}';
     }
 }
