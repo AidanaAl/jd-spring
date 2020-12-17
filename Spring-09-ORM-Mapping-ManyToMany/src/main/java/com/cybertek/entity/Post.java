c6b86d91c77401cb01f315e7c1cf09eacc638b7b
Aidana <aidanaalhomsi@gmail.com>
2020-12-16 21:19:46 -0500

    ORM-Mapping-ManyToMany

---
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
@@ -0,0 +1,39 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tilte;
+    private String description;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "post_tag_rel",
+            joinColumns = {@JoinColumn(name = "post_id")},
+            inverseJoinColumns = {@JoinColumn(name = "tag_id")} )
+    private Set<Tag> tags = new HashSet<>();
+
+
+
+    public Post(String tilte, String description) {
+        this.tilte = tilte;
+        this.description = description;
+    }
+
+
+
+}
