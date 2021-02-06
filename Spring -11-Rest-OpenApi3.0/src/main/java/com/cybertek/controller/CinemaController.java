8a73ddfe7475a08adc5827fd30c2ce1111d1e7a8
Aidana <aidanaalhomsi@gmail.com>
2021-02-06 10:22:26 -0500

    Rest-OpenAPiv3.0

---
+++ Spring -11-Rest-OpenApi3.0/src/main/java/com/cybertek/controller/CinemaController.java
@@ -0,0 +1,40 @@
+package com.cybertek.controller;
+
+
+import com.cybertek.entity.Cinema;
+import com.cybertek.entity.User;
+import com.cybertek.repository.CinemaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cinema")
+@Tag(name = "Cinema", description = "Cinema API")
+public class CinemaController {
+
+    @Autowired
+    private CinemaRepository cinemaRepository;
+
+    @GetMapping
+    @Operation(summary = "Read All Cinemas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Something went wrong", content = @Content)
+    })
+    public List<Cinema> readAll(){
+        return cinemaRepository.findAll();
+    }
+
+
+
+}
---
+++ Spring -11-Rest-OpenApi3.0/src/main/java/com/cybertek/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.cybertek.controller;
+
+import com.cybertek.entity.User;
+import com.cybertek.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@Tag(name = "User", description = "User API")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    @Operation(summary = "Read All Users")
+    public List<User> readAll(){
+        return userRepository.findAll();
+    }
+}
